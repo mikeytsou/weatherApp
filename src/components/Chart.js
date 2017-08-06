fae42fe7621e773d0669dc70b8474028268de4ef
Mike Tsou <mr.tsou@gmail.com>
2017-08-06 01:31:01 -0700

    Convert kelvin to fahrenheit for temperature unit measurment

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
-
+import _ from 'lodash';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
 const Chart = (props) => {
+  function average(data) {
+    return _.round(_.sum(data) / data.length);
+  }
+
   return (
     <div>
       <Sparklines height={80} width={150} data={props.data}>
         <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
+      <div>{average(props.data)} {props.units} </div>
     </div>
+
   );
 };
 
--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 import Chart from '../components/Chart';
 
 class WeatherList extends Component {
   // this function renders a single city(row)
   renderWeather(cityData) {
     const name = cityData.city.name;
-    const temperature = cityData.list.map((weather) => weather.main.temp);
+    // returns an array of temps, pressures, and humidities for each
+    const temperature = _.map(cityData.list.map((weather) => weather.main.temp), (temp) => temp * (9 / 5) - 459.67); // _.map converts to fahrenheit
     const pressure = cityData.list.map((weather) => weather.main.pressure);
     const humidity = cityData.list.map((weather) => weather.main.humidity);
 
@@ -14,12 +16,9 @@ class WeatherList extends Component {
     return (
       <tr key={name}>
         <td>{name}</td>
-
-        <td><Chart data={temperature} color="orange" /></td>
-
-        <td><Chart data={pressure} color="blue" /></td>
-
-        <td><Chart data={humidity} color="green" /></td>
+        <td><Chart data={temperature} color="orange" units="°F" /></td>
+        <td><Chart data={pressure} color="blue" units="hPa" /></td>
+        <td><Chart data={humidity} color="green" units="%" /></td>
       </tr>
     );
   }
@@ -30,9 +29,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th className="four wide">City</th>
-            <th className="four wide">Temperature</th>
-            <th className="four wide">Pressure</th>
-            <th className="four wide">Humidity</th>
+            <th className="four wide">Temperature (°F)</th>
+            <th className="four wide">Pressure (hPa)</th>
+            <th className="four wide">Humidity (%)</th>
           </tr>
         </thead>
 
