16ba0d58b5f7cb8d6ca08428041b2887c3c79941
Mike Tsou <mr.tsou@gmail.com>
2017-08-06 00:13:16 -0700

    Install sparklines module and create temperature charts

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 // import { bindActionCreators } from 'redux';
 // import { fetchWeather } from '../actions/index';
 
 class WeatherList extends Component {
+  // this function renders a single city(row)
   renderWeather(cityData) {
     const name = cityData.city.name;
+    const temperature = cityData.list.map((weather) => weather.main.temp);
+    const pressure = cityData.list.map((weather) => weather.main.pressure);
+    const humidity = cityData.list.map((weather) => weather.main.humidity);
+
 
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td>
+          <Sparklines height={120} width={180} data={temperature}>
+            <SparklinesLine color="red" />
+          </Sparklines>
+        </td>
       </tr>
     );
   }
